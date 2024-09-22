c8eb7b0821d668ed8a2d42ada165f0a6845e4187
Jacob Freeman <t00niyf5@duck.com>
2024-09-22 06:51:21 -0400

    Add infinite redirects and some cursed templating (and some good too)

--- app.py
+++ app.py
@@ -5,26 +5,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    return render_template("index.html")
+    return render_template("index.html", infinite=False)
 
 @app.route('/redirected/<redirect>')
 def redirected(redirect):
     return render_template("redirected.html", redir=redirect)
 
-@app.route('/meta')
-def meta():
-    return render_template("redirect.html", usemeta=True, usejavascript=False)
+@app.route('/meta/<int:inf>')
+def meta(inf):
+    return render_template("redirect.html", usemeta=True, usejavascript=False, inf=inf)
 
-@app.route('/java')
-def javascript():
-    return render_template("redirect.html", usejavascript=True, usemeta=False)
+@app.route('/java/<int:inf>')
+def javascript(inf):
+    return render_template("redirect.html", usejavascript=True, usemeta=False, inf=inf)
 
-@app.route('/code/<int:code>')
-def doCode(code):
+@app.route('/code/<int:code>/<int:inf>')
+def doCode(code, inf):
     response = make_response()
-    response.headers['location'] = url_for('redirected', redirect=code)
+    if inf:
+        response.headers['location'] = url_for('doCode', code=code, inf=1)
+    else:
+        response.headers['location'] = url_for('redirected', redirect=code)
     response.status = code
     return response
+
+@app.route('/infinite')
+def infinite():
+    return render_template("index.html", infinite=1)
 
 
 if __name__ == '__main__':
